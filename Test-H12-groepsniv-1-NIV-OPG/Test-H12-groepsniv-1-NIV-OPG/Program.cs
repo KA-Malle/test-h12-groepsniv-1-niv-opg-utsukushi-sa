f009fc7b9f61cf7c42b9398eccc5fe6b6b8bb5fe
utsukushi-sa <stuytskyan45@gmail.com>
2025-10-23 12:49:56 +0200

    test Kyan Stuyts opladen

--- Test-H12-groepsniv-1-NIV-OPG/Test-H12-groepsniv-1-NIV-OPG/Program.cs
+++ Test-H12-groepsniv-1-NIV-OPG/Test-H12-groepsniv-1-NIV-OPG/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,7 +12,7 @@ namespace Test_H12_groepsniv_1_NIV_OPG
         static void Main(string[] args)
         {
             /*
-             * NAAM => .............
+             * NAAM => Kyan Stuyts 6ADB 23/10/25
              */
 
             // Consolekleuren instellen.
@@ -19,6 +20,88 @@ namespace Test_H12_groepsniv_1_NIV_OPG
             Console.BackgroundColor = ConsoleColor.White;
             Console.Clear();
 
+            // Waarde volledige record lijn.
+
+            // Per record
+            string vorigeVertrekplaats = "", vertrekPlaats = ""; // Sleutel (primary key)
+            string vertrekCode;
+            decimal bezetting, capaciteit;
+
+
+            string vollediglijn;
+            string[] lijnGesplit;
+
+            // SreamReader starten en titel afdrukken 
+            Console.WriteLine("Bezetting vliegtuigen per vertrekpunt");
+            Console.WriteLine("=====================================");
+            Console.WriteLine("");
+
+            // Document lezen met streamreader.
+            using (StreamReader streamLees = new StreamReader("vliegtuigbezetting.txt"))
+            {
+                // Tellers op  0 zetten
+                decimal procent = 0;
+                bezetting = 0;
+                capaciteit = 0;
+                Int32 gecontroleerdeVlieg = 0;
+                Int32 rapportVerzonden = 0;
+                Int32 gemBezetting = 0;
+
+                // Lezen 1 lijn van een record.
+                vollediglijn = streamLees.ReadLine();
+                while (streamLees.Peek() != -1 )
+                {
+                    // Lezen een record
+                    vollediglijn = streamLees.ReadLine();
+                    lijnGesplit = vollediglijn.Split(',');
+
+
+                    // Gegevens records juist plaatsen
+                    vertrekCode = lijnGesplit[0];
+                    vertrekPlaats = lijnGesplit[1];
+                    bezetting = Convert.ToDecimal(lijnGesplit[2]);
+                    capaciteit = Convert.ToDecimal(lijnGesplit[3]);
+
+                 
+                    
+
+                    
+
+                    if (vorigeVertrekplaats != vertrekPlaats)
+                    {
+                        
+                            // Percentage per vliegtuig.
+                            procent = Math.Round((bezetting / capaciteit) * 100, 2, MidpointRounding.AwayFromZero);
+                            Console.WriteLine("Percentage: " + procent.ToString());
+                            Console.WriteLine("");
+
+                        /*
+                        // Zolang de vertrekplaats hetzelfde is alles optellen.
+                        while (vorigeVertrekplaats == vertrekPlaats)
+                        {
+                            bezetting += bezetting;
+                            capaciteit += capaciteit;
+                        }
+                        */
+
+
+                        // vertrekplaats wijzigen
+                        Console.WriteLine(vertrekPlaats);
+                       // test  Console.WriteLine(vorigeVertrekplaats)
+                        // test Console.WriteLine(bezetting);
+                       //  test Console.WriteLine(capaciteit);
+                        vorigeVertrekplaats = vertrekPlaats;
+                       gecontroleerdeVlieg= gecontroleerdeVlieg + 1;
+
+
+
+                    }
+
+
+
+
+                }
+            }
 
 
 
